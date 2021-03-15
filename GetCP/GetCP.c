51d15bdb2888e7eab74fe54a730c35ac7377fb7d
Creepercdn <creepercdn@outlook.com>
2021-03-15 21:44:59 +0800

    添加项目文件。

---
+++ GetCP/GetCP.c
@@ -0,0 +1,16 @@
+﻿#include <WinNls.h>
+#include <stdio.h>
+
+int main(int argc, char** argv)
+{
+    if (argv[1] == "acp") {
+        printf("%d", GetACP());
+    }
+    else if (argv[1] == "oemcp") {
+        printf("%d", GetOEMCP());
+    }
+    else {
+        print("Usage:\n%s (acp|oemcp)", argv[0]);
+    }
+    return 0;
+}
