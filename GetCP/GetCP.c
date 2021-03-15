990d100c9feb04a367e6b4213fe9795705a10e1f
Creepercdn <creepercdn@outlook.com>
2021-03-15 22:06:48 +0800

    fix bugs again

--- GetCP/GetCP.c
+++ GetCP/GetCP.c
@@ -4,6 +4,10 @@
 
 int main(int argc, char** argv)
 {
+    if (argc < 2) {
+        printf("Usage:\n%s (acp|oemcp)", argv[0]);
+        return 0;
+    }
     if (strcmp(argv[1],"acp")==0) {
         printf("%d", GetACP());
     }
